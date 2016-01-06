8d96bd368f44bb24f2ea856916d9810c99ae6571
Daisuke Shimamoto <diskshima@gmail.com>
2016-01-06 17:10:40 +0900

    Add code using OptionParser

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule BbCli.Mixfile do
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     escript: [main_module: BbCli],
      deps: deps]
   end
 

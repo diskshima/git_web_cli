b6460e715ecab3ab10259d6488e669760a152e0c
Daisuke Shimamoto <diskshima@gmail.com>
2016-01-17 22:27:07 +0900

    Add method to extract project ID
    The 'project_id' uses the search function in GitLab as the server we
    have isn't working when the URL contains a '/' (slash).

--- lib/gitlab.ex
+++ lib/gitlab.ex
@@ -10,6 +10,24 @@ defmodule GitLab do
     resource.body
   end
 
-  def project_id do
+  def project_id(gl) do
+    [path, name] = gl.repo |> String.split("/")
+
+    search_repo_name(name)
+    |> Enum.filter(&(&1["namespace"]["path"] == path))
+    |> Enum.at(0)
+    |> Dict.get("id")
+  end
+
+  def search_repo_name(query) do
+    token = oauth2_token
+    get_resource!("/projects/search/" <> query)
+  end
+
+  defp extract_repo_names(urls) do
+    urls
+    |> Enum.map(&Regex.replace(~r/^git@bitbucket.org:(.*).git$/, &1, "\\g{1}"))
+    |> Enum.map(&Regex.replace(~r/^https:\/\/.+@bitbucket.org\/(.*).git$/, &1,
+         "\\g{1}"))
   end
 end

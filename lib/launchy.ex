aa9b0e6f7699533f8064d632778c3f8967dbd426
Daisuke Shimamoto <diskshima@gmail.com>
2016-01-24 17:01:30 +0900

    Add support for Linux (XFCE only)

--- lib/launchy.ex
+++ lib/launchy.ex
@@ -4,12 +4,32 @@ defmodule Launchy do
   """
 
   def open_url(url) do
-    if osx? do
-      System.cmd("open", [url])
+    cond do
+      osx? -> System.cmd("open", [url])
+      linux? -> open_on_linux(url)
+    end
+  end
+
+  defp open_on_linux(url) do
+    {command, args} = linux_command
+    System.cmd(command, args ++ [url])
+  end
+
+  defp linux_command do
+    case System.get_env("XDG_CURRENT_DESKTOP") do
+      "XFCE" -> {"exo-open", ["--launch", "WebBrowser"]}
     end
   end
 
   defp osx? do
-    :os.type |> elem(1) == :darwin
+    compare_os_type(:darwin)
+  end
+
+  defp linux? do
+    compare_os_type(:linux)
+  end
+
+  defp compare_os_type(type) do
+    :os.type |> elem(1) == type
   end
 end

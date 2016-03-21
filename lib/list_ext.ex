6bb438114e530f7a174f9b41d0a0aecb0be7ee84
Daisuke Shimamoto <diskshima@gmail.com>
2016-03-21 19:21:31 +0900

    Extend ListExt.pop to support count

--- lib/list_ext.ex
+++ lib/list_ext.ex
@@ -1,9 +1,22 @@
 defprotocol ListExt do
   def pop(list)
+  def pop(list, count)
 end
 
 defimpl ListExt, for: List do
   def pop(list) do
+    pop(list, 1)
+  end
+
+  def pop(list, count) do
+    pop_in(list, count, {})
+  end
+
+  defp pop_in(list, 0, acc) do
+    Tuple.append(acc, list)
+  end
+
+  defp pop_in(list, count, acc) do
     item = list |> List.first
 
     rest = if item do
@@ -12,6 +25,6 @@ defimpl ListExt, for: List do
         []
       end
 
-    {item, rest}
+    pop_in(rest, count - 1, Tuple.append(acc, item))
   end
 end

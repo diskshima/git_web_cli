6d8369df3416809a61e38a1b6ddcede0de49e8d6
Daisuke Shimamoto <diskshima@gmail.com>
2016-03-21 19:28:03 +0900

    Add save_key/read_key to Config

--- lib/config.ex
+++ lib/config.ex
@@ -24,4 +24,35 @@ defmodule Config do
 
     File.write(gw_file, json, [:write])
   end
+
+  def read_key(key) when is_atom(key) do
+    read_key(Atom.to_string(key))
+  end
+
+  def read_key(key) do
+    case read do
+      {:ok, content} -> content[key]
+      {:error, reason} -> raise "Failed to retrieve key."
+    end
+  end
+
+  def save_key(key, content) when is_atom(key) do
+    save_key(Atom.to_string(key), content)
+  end
+
+  def save_key(key, content) do
+    converted = content |> Utils.stringify_keys
+
+    new_content =
+      case read do
+        {:ok, existing} ->
+          values = Dict.merge(existing[key], converted)
+          Map.put(existing, key, values)
+        {:error, _} -> content
+      end
+
+    json = Poison.encode!(new_content, [pretty: true])
+
+    File.write(gw_file, json, [:write])
+  end
 end
